69a2706d2ab2cd0b74dc2d8362ffeea61b36deaf
Mori656 <marcelnosek@o2.pl>
2026-01-12 16:53:16 +0100

    markest is now function

--- marktest.py
+++ marktest.py
@@ -3,24 +3,41 @@ from MarkdownLexer import MarkdownLexer
 from MarkdownParser import MarkdownParser
 from HtmlVisitor import MarkdownToHtmlVisitor as HtmlVisitor
 
-input_stream = FileStream("test.md", encoding="utf-8")
-lexer = MarkdownLexer(input_stream)
-tokens = CommonTokenStream(lexer)
+def test_markdown_parsing():
+    input_stream = FileStream("test.md", encoding="utf-8")
+    lexer = MarkdownLexer(input_stream)
+    tokens = CommonTokenStream(lexer)
 
-parser = MarkdownParser(tokens)
-tree = parser.document()
+    parser = MarkdownParser(tokens)
+    tree = parser.document()
 
-tokens.fill()
+    tokens.fill()
 
-for token in tokens.tokens:
-    print(
-        lexer.symbolicNames[token.type],
-        repr(token.text)
-    )
+    for token in tokens.tokens:
+        print(
+            lexer.symbolicNames[token.type],
+            repr(token.text)
+        )
 
-visitor = HtmlVisitor()
-print(tree.toStringTree(recog=parser))
-html = visitor.visit(tree)
+    visitor = HtmlVisitor()
+    print(tree.toStringTree(recog=parser))
+    html = visitor.visit(tree)
+    print(html)
 
-print("\nGenerated HTML:\n")
-print(html)
+def markdown_to_html(markdown: str) -> str:
+    input_stream = InputStream(markdown)
+
+    lexer = MarkdownLexer(input_stream)
+    tokens = CommonTokenStream(lexer)
+
+    parser = MarkdownParser(tokens)
+    tree = parser.document()
+
+    visitor = MarkdownToHtmlVisitor()
+    html = visitor.visit(tree)
+
+    return html
+
+if __name__ == "__main__":
+    print("Running markdown parser ...")
+    test_markdown_parsing()
